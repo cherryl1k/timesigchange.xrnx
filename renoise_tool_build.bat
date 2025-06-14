6bce0c102c525d4f2c6c2d358cd5437d2d5d48a2
chrymusic <102343489+chrymusic@users.noreply.github.com>
2025-06-14 12:34:32 -0500

    finally fixed it

--- renoise_tool_build.bat
+++ renoise_tool_build.bat
@@ -2,11 +2,17 @@
 set directory=src
 set output=com.timesigchange
 
-:: Compress src into tool.zip
-tar -a -c -f %output%.zip %directory%
+:: Remove previous files if they exist
+if exist %output%.xrnx del %output%.xrnx
+if exist %output%.zip del %output%.zip
 
-:: Rename tool.zip to tool.xrnx
+:: Compress src's content directly into a .zip
+cd %directory%
+tar -a --options compression-level=0 -c -f ../%output%.zip *
+cd ..
+
+:: Rename .zip to .xrnx
 rename %output%.zip %output%.xrnx
 
-echo Done! %output%.xrnx has been created.
+echo Done! %output%.xrnx has been created, replacing any previous files.
 pause
